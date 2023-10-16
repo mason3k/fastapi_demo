deedd740efb81f063dc509b6efa5195d3bc80325
Sydney Jones <sydney.u.jones@gmail.com>
2023-10-16 10:33:40 -0500

    ✨ introduce put and delete

--- src/db.py
+++ src/db.py
@@ -10,13 +10,11 @@ from .model import HotSauce
 @dataclass
 class MockDb:
     SRC_FILE: ClassVar[Path] = Path(__file__).parent / "data.csv"
-
-    items: list[HotSauce] = field(init=False)
     items_by_id: dict[int, HotSauce] = field(init=False)
 
     def __post_init__(self):
-        self.items = self._load_data()
-        self.items_by_id = {entry.id: entry for entry in self.items}
+        items = self._load_data()
+        self.items_by_id = {entry.id: entry for entry in items}
 
     def _load_data(self) -> list[HotSauce]:
         sauces = []
@@ -31,5 +29,4 @@ class MockDb:
     def add(self, sauce: HotSauce):
         if self.items_by_id.get(sauce.id):
             raise ValueError(f"Error: id {sauce.id} is already in use")
-        self.items.append(sauce)
         self.items_by_id[sauce.id] = sauce
--- src/main.py
+++ src/main.py
@@ -21,24 +21,38 @@ async def create_item(sauce: HotSauce) -> HotSauce:
 
 @app.get("/sauces/")
 async def read_items() -> list[HotSauce]:
-    return DB.items
+    return list(DB.items_by_id.values())
 
 
-@app.get("/sauces/{sauce_id}/")
-async def read_item(sauce_id: int):
-    if sauce_id not in DB.items_by_id:
+@app.get("/sauces/{sauce_id}")
+async def read_item(sauce_id: int) -> HotSauce:
+    if not (sauce := DB.items_by_id.get(sauce_id)):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Sauce with id {sauce_id} does not exist",
         )
-    return DB.items_by_id[sauce_id]
+    return sauce
 
 
-@app.put("/sauces/{sauce_id}/")
-async def update_item(sauce_id, sauce) -> HotSauce:
-    ...
+@app.put("/sauces/{sauce_id}")
+async def update_item(sauce_id: int, new_sauce: HotSauce) -> HotSauce:
+    if not (existing_sauce := DB.items_by_id.get(sauce_id)):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Sauce with id {sauce_id} does not exist",
+        )
+    updated_sauce = existing_sauce.model_copy(update=new_sauce.model_dump(exclude_defaults=True))
+    DB.items_by_id[sauce_id] = updated_sauce
+
+    return updated_sauce
 
 
-@app.delete("/sauces/{sauce_id}")
-async def delete_item(sauce_id):
-    ...
+@app.delete("/sauces/{sauce_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_item(sauce_id: int) -> dict:
+    if sauce_id not in DB.items_by_id:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"sauce with id {sauce_id} does not exist",
+        )
+    del DB.items_by_id[sauce_id]
+    return {}
